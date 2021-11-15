54806eb59c127fb4f2d10892a3da24fa48027eb8
Hilmi Hidayat <hilmihidayat175@gmail.com>
2021-11-15 23:50:12 +0700

    add pagination or load more pages

--- app/Http/Livewire/Tweets/Index.php
+++ app/Http/Livewire/Tweets/Index.php
@@ -4,13 +4,24 @@ namespace App\Http\Livewire\Tweets;
 
 use App\Models\Tweet;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Index extends Component
 {
+    use WithPagination;
+
+    protected $paginationTheme = 'bootstrap';
+
+    public $perPage = 10;
+
+    public function loadMore()
+    {
+        $this->perPage += 10;
+    }
 
     public function render()
     {
-        $tweets = Tweet::latest()->get();
+        $tweets = Tweet::latest()->paginate($this->perPage);
 
         return view('livewire.tweets.index', compact('tweets'));
     }
--- resources/views/livewire/tweets/index.blade.php
+++ resources/views/livewire/tweets/index.blade.php
@@ -7,6 +7,11 @@
             @foreach ($tweets as $tweet)
                 @livewire('tweets.single', ['tweet' => $tweet], key($tweet->id))
             @endforeach
+
+            {{-- {{ $tweets->links() }} //use pagination --}}
+            @if ($tweets->hasMorePages())
+                <button class="btn btn-primary" wire:click.prevent="loadMore">Load More</button>
+            @endif
         </div>
     </div>
 </div>
